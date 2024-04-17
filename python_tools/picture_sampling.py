dd20dd3133851fc683ddc63a945f80aca1101939
이원준 <evenil0206@gmail.com>
2024-04-17 15:13:42 +0900

    another python tools

--- python_tools/picture_sampling.py
+++ python_tools/picture_sampling.py
@@ -2,14 +2,16 @@ import random
 import shutil
 import os
 
-def sampling(source_url, destination_url, source_boundary) :
-    sampling_tbl = [i for i in range(source_boundary)]
-    for i in range(0, 500) :
-        t = random.randint(i, source_boundary - 1)
+def sampling(source_url, destination_url, start_number, last_frame_number) :
+    width = last_frame_number - start_number + 1
+    sample_size = 500
+    sampling_tbl = [i for i in range(width)]
+    for i in range(0, sample_size) :
+        t = random.randint(i,  width - 1)
         sampling_tbl[i], sampling_tbl[t] = sampling_tbl[t], sampling_tbl[i]
         
-        file_name = os.path.join(source_url, f'{sampling_tbl[i]:06d}.jpg')
-        dest_name = os.path.join(destination_url, f'{i:06d}.jpg')
+        file_name = os.path.join(source_url, f'{sampling_tbl[i] + start_number:06d}.jpg')
+        dest_name = os.path.join(destination_url, f'{i+500:06d}.jpg')
         print(file_name, dest_name)
         try :
             shutil.move(file_name, dest_name)
@@ -17,4 +19,4 @@ def sampling(source_url, destination_url, source_boundary) :
             print(e)
 
 if __name__ == '__main__' :
-    sampling('D:\\captured_pics', 'D:\\training_datas\\training_image_samples', 857)
+    sampling('D:\\captured_pics', 'D:\\training_datas\\training_image_samples',start_number = 630, last_frame_number= 2107)
